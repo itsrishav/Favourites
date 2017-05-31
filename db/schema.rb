624bde266915c00409337aa1e3a27e2371af8e4f
itsrishav <with.rishav@gmail.com>
2017-05-31 10:36:07 +0530

    database changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170530113740) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "saved_titles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
